846e7ac2a9eb2eefdd67e7e672b3625dad25ebe1
rphi8796 <jlkfop@gmail.com>
2018-09-11 12:33:45 -0600

    I added the model and a bunch of variable names

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,22 @@
 package controller;
 
+import model.MarshmallowMonster;
+
 public class Controller
 {
-
+	//Data member section
+	//Declaration section
+	private MarshmallowMonster myMonster;
+	
+	//Constructors initialize data members
+	public Controller()
+	{
+		myMonster = new MarshmallowMonster("Jeef");
+	}
+	
+	
+	public void start()
+	{
+		System.out.println(myMonster);
+	}
 }
---
+++ src/model/MarshmallowMonster.java
@@ -0,0 +1,32 @@
+package model;
+
+public class MarshmallowMonster
+{
+	//Declaration section
+	private String name;
+	private double legCount;
+	private int eyeCount;
+	private boolean hasNoses;
+	private int armCount;
+	
+	
+	public MarshmallowMonster()
+	{	
+		//Default constructor
+		//Generally boring and no as helpful
+	}
+	
+	public MarshmallowMonster(String name)
+	{
+		this.name = name;
+	}
+	
+	
+	
+	public String toString()
+	{
+		String description = "";
+		
+		return description;
+	}
+}
