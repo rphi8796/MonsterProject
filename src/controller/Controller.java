960990d04b3efdac04fe74dc038341bb26780677
rphi8796 <jlkfop@gmail.com>
2018-09-18 13:20:18 -0600

    I added some control structures to give different responses. Mr H also helped me fixed some local variable issues. I still need to fix my local variable: conditionalMessage.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,7 @@
 package controller;
 
 import model.MarshmallowMonster;
-import java.util.Scanner;
+//import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 
@@ -9,21 +9,40 @@ public class Controller
 {
 	//Data member section
 	//Declaration section
-	private MarshmallowMonster myMonster;
+	//private MarshmallowMonster myMonster;
 	private MarshmallowMonster userMonster;
-	private Scanner inputScanner;
+
 	
 	//Constructors initialize data members
 	public Controller()
 	{
 		//myMonster = new MarshmallowMonster("Jeef", "2.2", "5", "false", "4");
 		userMonster = new MarshmallowMonster();
-		inputScanner =  new Scanner(System.in);
+		//inputScanner =  new Scanner(System.in);
 		
 	}
 	
 	public void start()
 	{
+		
+		//String name = inputScanner.next();
+		
+		String name = JOptionPane.showInputDialog(null, "what is your monster's name?");
+		userMonster.setName(name);
+		
+		if(name.equals("Ryan"))
+		{
+			JOptionPane.showMessageDialog(null, "Oh whoa that is a fantastic name.");
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null,"Hmmm, I guess I was right not to be threatened by you.");
+		}
+				
+		//System.out.println("Your monsters name is " + userMonster.getName() + " name");
+				
+		//boolean nose = inputScanner.nextInt();
+		
 		//JOptionPane.showMessageDialog(null,  myMonster);
 		//System.out.println(myMonster);
 		
@@ -39,9 +58,9 @@ public class Controller
 			arms = Integer.parseInt(userInput);
 		}
 		
-		boolean answer = Boolean.parseBoolean(userInput);
 		
 		userMonster.setArmCount(arms);
+		JOptionPane.showMessageDialog(null, userMonster.getName() + " has " + userMonster.getArmCount() + " arms");
 		//System.out.println("Your monster has " + userMonster.getArmCount() + " arms");
 		
 		
@@ -54,7 +73,15 @@ public class Controller
 			legs = Integer.parseInt(userInput2);
 		}
 		
-		boolean answer2 = Boolean.parseBoolean(userInput2);
+		if(legs >= 100)
+		{
+			String conditionalMessage = "Wow, your monster must be great at charades!";
+		}
+		else 
+		{
+			String conditionalMessage = "That is very sensible";
+		}
+		JOtionPane.showMessageDialog(null, "Your monster has" + userMonster.getArmCount() + ". " + conditionalMessage);
 		
 		userMonster.setLegCount(legs);
 		//System.out.println("Your monster has " + userMonster.getLegCount() + " legs");
@@ -69,25 +96,16 @@ public class Controller
 			eyes = Integer.parseInt(userInput3);
 		}
 		
-		boolean answer3 = Boolean.parseBoolean(userInput3);
 		
 		userMonster.setEyeCount(eyes);
 		
 		//System.out.println("Your monster has" + userMonster.getEyeCount() + " eyes");
 		
-		//String name = inputScanner.next();
 		
-		String userInput4 = JOptionPane.showInputDialog(null, "what is your monster's name?");
-		String name = "jeef";
-		userMonster.setName(name);
-		
-		//System.out.println("Your monsters name is " + userMonster.getName() + " name");
-		
-		//boolean nose = inputScanner.nextInt();
 		
 		String userInput5 = JOptionPane.showInputDialog(null, "Is it true or false that your monster has a nose?");
 		boolean nose = false;
-		
+		nose = Boolean.parseBoolean(userInput5);
 		userMonster.setHasNoses(nose);
 		//System.out.println("Your monster has a " + userMonster.getEyeCount() + " nose");
 		
